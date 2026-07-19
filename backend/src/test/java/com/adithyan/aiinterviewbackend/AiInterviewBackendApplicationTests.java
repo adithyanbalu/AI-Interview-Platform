568753b99f95eb3872c9ca92452e37f518d849eb
Adithyan B <adithyanbalu8@gmail.com>
2026-07-19 17:44:01 +0530

    chore: initialize Spring Boot backend with PostgreSQL

---
+++ backend/src/test/java/com/adithyan/aiinterviewbackend/AiInterviewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adithyan.aiinterviewbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInterviewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
