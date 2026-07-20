050181d36df058cc97a6294c9add014ccbe94546
Adithyan B <adithyanbalu8@gmail.com>
2026-07-20 10:51:48 +0530

    feat(user): implement User module with GET and POST APIs

---
+++ backend/src/main/java/com/adithyan/aiinterviewbackend/user/UserController.java
@@ -0,0 +1,27 @@
+package com.adithyan.aiinterviewbackend.user;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
